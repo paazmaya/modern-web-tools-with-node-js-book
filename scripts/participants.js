32d3bda8e8083951b8b224fedcd552bc5823e69d
Juga Paazmaya <paazmaya@yahoo.com>
2014-09-23 19:03:01 +0300

    Get them names

--- scripts/participants.js
+++ scripts/participants.js
@@ -15,17 +15,109 @@ var pkg = JSON.parse(fs.readFileSync('package.json'));
 
 var gh = new github({
   version: '3.0.0',
-  debug: true
+  debug: false
 });
-
-console.log(pkg);
-console.log('name: ' +pkg.name);
 
 gh.authenticate({
   type: 'oauth',
   token: process.env.GITHUB_TOKEN
 });
 
-gh.issues.repoIssues({user: 'paazmaya', repo: pkg.name}, function (error, data) {
-  console.dir(data);
-});
+var options = {
+  user: 'paazmaya',
+  repo: pkg.name,
+  state: 'all',
+  per_page: 100
+};
+
+var names = [];
+
+
+var fetchIssueComment = function () {
+  var filename = 'issue-comments.json';
+  gh.issues.repoComments(options, function (error, data) {
+    console.log(data.length);
+
+    fs.writeFileSync(filename, JSON.stringify(data, null, '  '));
+  });
+};
+
+var fetchIssues = function () {
+  var filename = 'issues.json';
+  gh.issues.repoIssues(options, function (error, data) {
+    console.log(data.length);
+
+    fs.writeFileSync(filename, JSON.stringify(data, null, '  '));
+  });
+};
+
+var fetchPullRequests = function () {
+  var filename = 'pull-requests.json';
+  gh.pullRequests.getAll(options, function (error, data) {
+    console.log(data.length);
+
+    fs.writeFileSync(filename, JSON.stringify(data, null, '  '));
+  });
+};
+
+/**
+ * Get list of unique usernames
+ * @param {array} data
+ */
+var prUsers = function () {
+  var filename = 'pull-requests.json';
+  var data = JSON.parse(fs.readFileSync(filename));
+
+  var list = {};
+  data.forEach(function (item) {
+    list[item.user.login] = item.user.html_url;
+  });
+  return Object.keys(list);
+};
+
+
+/**
+ * Get list of unique usernames
+ * @param {array} data
+ */
+var issueUsers = function () {
+  var filename = 'issues.json';
+  var data = JSON.parse(fs.readFileSync(filename));
+
+  var list = {};
+  data.forEach(function (item) {
+    list[item.user.login] = item.user.html_url;
+  });
+  return Object.keys(list);
+};
+
+/**
+ * Get list of unique usernames
+ * @param {array} data
+ */
+var issueCommentUsers = function () {
+  var filename = 'issue-comments.json';
+  var data = JSON.parse(fs.readFileSync(filename));
+
+  var list = {};
+  data.forEach(function (item) {
+    list[item.user.login] = item.user.html_url;
+  });
+  return Object.keys(list);
+};
+
+
+var mergeNames = function () {
+  var names = [].concat(
+    prUsers(),
+    issueUsers(),
+    issueCommentUsers()
+  );
+
+  return names.filter(function (item, index, list) {
+    return list.lastIndexOf(item) > index;
+  });
+};
+
+names = mergeNames();
+console.log(names.length);
