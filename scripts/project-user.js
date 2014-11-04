f16f6fce8e80ed40c4b2a89ad4e847aca70af23a
Juga Paazmaya <paazmaya@yahoo.com>
2014-11-04 14:30:52 +0200

    Presentation and testing script

---
+++ scripts/project-user.js
@@ -0,0 +1,65 @@
+/**
+ * Get the base directory where project assignments are,
+ * and try to use them programmatically.
+ */
+'use strict';
+
+var fs = require('fs'),
+  path = require('path'),
+  util = require('util');
+
+if (process.argv.length !== 3) {
+  util.error('Project checkout base directory must be defined');
+  process.exit();
+}
+
+
+var getDirs = function (baseDir) {
+  return fs.readdirSync(baseDir).filter(function (filepath) {
+    var stat = fs.statSync(baseDir + '/' + filepath);
+    return stat && stat.isDirectory();
+  });
+};
+
+var readPackages = function (users, baseDir) {
+
+  var packages = {};
+  users.forEach(function (user) {
+    var list = getDirs(baseDir + '/' + user);
+    list.forEach(function (item) {
+      var meta = user + '/' + item + '/package.json';
+      if (fs.existsSync(meta)) {
+        var json = fs.readFileSync(meta, 'utf8');
+        var data = JSON.parse(json);
+        packages[data.name] = './' + user + '/' + item;
+      }
+    });
+  });
+  return packages;
+};
+
+var requirePackages = function (packages) {
+  Object.keys(packages).forEach(function (key) {
+    var index = packages[key];
+    util.puts(key + ' - ' + index);
+
+    try {
+      var ins = require(index);
+    }
+    catch (error) {
+      util.puts(error);
+    }
+    util.puts();
+  });
+};
+
+var baseDir = String(process.argv.slice(2));
+util.log(baseDir);
+
+var users = getDirs(baseDir);
+var packages = readPackages(users, baseDir);
+
+util.puts(util.inspect(packages));
+
+requirePackages(packages);
+
